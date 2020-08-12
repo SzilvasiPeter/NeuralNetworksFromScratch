765b9b2738ac12584039cd9f497469b565bd036f
SzilvasiPeter <peti.szilvasi95@gmail.com>
2020-08-12 15:09:47 +0200

    Simulate a input layer with 3 neuron and a output layer with 1 neuron

--- NeuralNetworksFromScratch/Program.cs
+++ NeuralNetworksFromScratch/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace NeuralNetworksFromScratch
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly List<double> Inputs = new List<double>()
         {
-            Console.WriteLine("Hello World!");
+            1, 2, 3
+        };
+        private static readonly List<double> Weights = new List<double>()
+        {
+            0.2, 0.8, -0.5
+        };
+
+        private static readonly double Bias = 2;
+
+        public static void Main(string[] args)
+        {
+            double output = Inputs[0] * Weights[0] + Inputs[1] * Weights[1] + Inputs[2] * Weights[2] + Bias;
+            Console.WriteLine(output);
         }
     }
 }
