8ffba73a082d112bbe6b28d63ad1637d2f3a01e3
Peter Szilvasi <peti.szilvasi95@gmail.com>
2022-07-31 14:40:02 +0200

    Adds basic neural network class and layers

---
+++ Python/layers.py
@@ -0,0 +1,131 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+epsilon = 10 ** -3
+
+class Layer(ABC):
+
+    @abstractmethod
+    def forward(self, x: np.ndarray) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def backward(self, grad_input: np.ndarray) -> np.ndarray:
+        pass
+
+
+class ParamLayer(Layer, ABC):
+
+    @abstractmethod
+    def apply_gradients(self, learning_rate: float) -> None:
+        pass
+
+
+class LinearLayer(ParamLayer):
+    
+    def __init__(self, input_dim, output_dim):
+        self.weights = np.random.normal(0.0, 0.1, size=input_dim*output_dim).reshape(input_dim, output_dim)
+        self.biases = np.zeros((1, output_dim))
+
+        self.x = np.zeros(0)
+
+        self.grad_weights = np.zeros(0)
+        self.grad_biases = np.zeros(0)
+
+    def forward(self, x):
+        self.x = x
+        return (self.x @ self.weights) + self.biases
+
+    def backward(self, grad_input):
+        self.grad_weights = self.x.T @ grad_input
+        self.grad_biases = np.sum(grad_input, axis=0, keepdims=True)
+        return (grad_input @ self.weights.T)
+
+    def apply_gradients(self, learning_rate):
+        self.weights -= learning_rate * self.grad_weights
+        self.biases -= learning_rate * self.grad_biases
+
+
+class SigmoidLayer(Layer):
+
+    def __init__(self):
+        self.activation = np.zeros(0)
+
+    def forward(self, x):
+        self.activation = 1 / (1 + np.exp(-x))
+        return self.activation
+
+    def backward(self, grad_input):
+        return (self.activation * (1 - self.activation)) * grad_input
+
+
+class RectifiedLinearUnitsLayer(Layer):
+
+    def __init__(self):
+        self.activation = np.zeros(0)
+
+    def forward(self, x):
+        self.activation = np.max(0, x)
+        return self.activation
+
+    def backward(self, grad_input):
+        return grad_input if self.activation > 0 else 0
+
+
+class SquaredErrorLayer(Layer):
+
+    def __init__(self, y):
+        self.y = y
+        self.x = np.zeros(0)
+
+    def forward(self, x):
+        self.x = x
+        return np.square(x - self.y)
+
+    def backward(self, grad_input):
+        return 2.0 * (self.x - self.y) * grad_input
+
+
+class SoftmaxLayer(Layer):
+
+    def __init__(self, y):
+        self.y = y
+        self.x = np.zeros(0)
+
+    def forward(self, x):
+        self.x = np.exp(x) / np.sum(np.exp(x) + 0.0001, axis=1, keepdims=True)
+        return self.x
+
+    def backward(self, grad_input):
+        softmax = self.next_x.reshape(-1,1)
+        return grad_input * (np.diagflat(softmax) - np.dot(softmax, softmax.T))
+
+
+class CrossEntropyLoss(Layer):
+
+    def __init__(self, y):
+        self.y = np.clip(y, epsilon, 1.0 - epsilon)
+        self.p = np.zeros(0)
+
+    def forward(self, x):
+        self.p = np.clip(x, epsilon, 1.0 - epsilon)
+        return -(self.y * np.log(self.p) + (1-self.y) * np.log(1 - self.p))
+
+    def backward(self, grad_input):
+        layer_error = (self.p - self.y) / (self.p - self.p ** 2)
+        return layer_error * grad_input
+
+
+class SoftMaxCrossEntropyLoss(Layer):
+
+    def __init__(self, y):
+        self.y = np.clip(y, epsilon, 1.0 - epsilon)
+        self.x = np.zeros(0)
+
+    def forward(self, x):
+        self.x = np.clip(x, epsilon, 1.0 - epsilon) 
+        return -(np.sum(self.y * np.log(x), axis=0))
+
+    def backward(self, grad_input):
+        layer_error = self.x - self.y
+        return layer_error * grad_input
--- Python/neural_network.py
+++ Python/neural_network.py
@@ -1,67 +1,57 @@
 from abc import abstractmethod
+from typing import Union
 import numpy as np
+import tqdm
+
+from layers import Layer, ParamLayer, SoftMaxCrossEntropyLoss, SoftmaxLayer
 
 
 class NeuralNetwork():
     def __init__(self, layers):
-        pass
+        self.layers = layers
+        self.loss_layer: Union[Layer, None] = None
 
-    def predict(self, X, y):
-        pass
+    def predict(self, X):
+        for layer in self.layers:
+            output = layer.forward(X)
+        return output
 
-    def fit(self, X_train, y_train, epochs=1000, lr=0.01):
-        pass
+    def fit(self, X_train, y_train, x_test, y_test, epochs=1000, learning_rate=0.01):
+        metrics = np.zeros((epochs, 2))  # loss, accuracy
+        self.loss_layer = SoftMaxCrossEntropyLoss(y_train)
+        for epoch in tqdm(range(epochs)):
+            y_pred = self.predict(X_train)
+            self.loss_layer.forward(y_pred)
+            self.backward_propagation(y_pred)
+            self.update_params(learning_rate)
+
+            loss, accuracy = self.evaluate_model(x_test, y_test)
+            metrics[epoch, :] = loss, accuracy
+
+        return metrics
 
     def backward_propagation(self, output):
-        pass
+        grad_input = self.loss_layer.backward(np.ones_like(output))
+        for layer in reversed(self.layers):
+            grad_input = layer.backward(grad_input)
 
-    def update_params(self, lr):
-        pass
+    def update_params(self, learning_rate):
+        for layer_index in range(len(self.layers)):
+            layer = self.layers[layer_index]
+            if isinstance(layer, ParamLayer):
+                layer.apply_gradients(learning_rate)
 
     def  evaluate(self, X_test, y_test):
-        pass
+        self.loss_layer = SoftmaxLayer(y_test)
+        y_pred = self.predict(X_test)
 
+        y_pred_labels = np.argmax(y_pred, axis=1)
+        y_labels = np.argmax(y_test, axis=1)
 
-class Layer():
-    pass
-
-
-class LinearLayer(Layer):
-    pass
-
-
-class Sigmoid(Layer):
-    pass
-
-
-class ReLU(Layer):
-    pass
-
-
-class BatchNorm(Layer):
-    pass
-
-
-class Softmax(Layer):
-    pass
-
-
-class Optimizer():
-    pass
-
-
-class ElasticReg():
-    pass
-
-
-class GradientDescent(Optimizer):
-    pass
-
-
-class ADAM(Optimizer):
-    pass
+        loss = self.loss_layer.forward(y_pred)
+        accuracy = np.sum(y_labels == y_pred_labels) / X_test.shape[0]
+        return float(np.mean(loss)), accuracy
 
 
 if __name__ == '__main__':
-    neural_network = NeuralNetwork()
-    print('End')
+    pass
